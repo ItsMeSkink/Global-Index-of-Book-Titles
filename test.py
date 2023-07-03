16bc9e936ee3a09adda482fbc9ad2227de74dc64
Lakshya Pratap Monga <wickedskink@gmail.com>
2023-07-04 00:41:15 +0530

    GIBT Draft 3 complete (1)

--- test.py
+++
@@ -1,9 +0,0 @@
-import json
-
-
-filename = 'AnalysisData\AbeBooks\Wings of Fire by Kalam.json'
-with open(filename, 'r') as readFile:
-    jsonData = json.loads(readFile.read())
-    settedData = list(set(jsonData))
-    with open(filename, 'w') as writeFile:
-        writeFile.write(str(json.dumps(settedData)))
